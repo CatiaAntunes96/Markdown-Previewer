9fe45a8bc50665d80db8f75cd287abd54b5d1f87
Catia Antunes <klisar1996@gmail.com>
2020-01-31 22:01:06 +0000

    Try to add markdown parse

--- src/Components/Previewer.js
+++ src/Components/Previewer.js
@@ -1,12 +1,12 @@
 import React from "react";
+import marked from "react-markdown";
 
 const Previewer = props => {
     
-
+   
     return (
-        <div className="right-side">
-            <button onClick={props.click}>click</button>
-            <p id="preview" >{props.value}</p>
+        <div>
+            <p id="preview" dangerouslySetInnerHTML={props.dangerouslySetInnerHTML}></p>
         </div>
     )
 }
--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -1,26 +1,29 @@
 import React, {useState} from "react";
 import Previewer from "./Previewer";
+import marked from "react-markdown";
 
 const Textarea = () => {
     const [text, setText] = useState("");
 
     const handleChange = (event) => {
-        setText(event.target.text)
+        setText(event.target.value)
         console.log(text)
     }
 
-    const click = () => {
-        setText(text)
-        //console.log()
+    const createMarkup = () => {
+        return { __html: marked(text) }
+    }
+    const getMarkdownText =  () => {
+        return { __html: createMarkup() };
     }
 
     return (
         <div>
         <div className="left-side">
-            <textarea id="editor" value={text} onChange={handleChange}/>
+            <textarea id="editor" value={text} onChange={handleChange} placeholder="Enter Markdown"/>
         </div>
-        <div>
-            <Previewer click={click} value={text}/>
+        <div className="right-side">
+            <Previewer text={text} dangerouslySetInnerHTML={getMarkdownText}/>
         </div>
         </div>
     )
